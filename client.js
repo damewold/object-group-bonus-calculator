5002d1ee2e7386f50c406b3c7986da871d87f517
Erick Jensen <Erick.Jensen010@gmail.com>
2020-03-03 16:53:58 -0600

    Calculator

--- client.js
+++ client.js
@@ -40,4 +40,40 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+
+
+//You are going to write a single JavaScript file to automatically calculate employee bonus for a company.
+//Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
+
+let newArray = [];
+
+
+
+  // let newObject = {
+  //   Name : employees[0].name,
+  //   bonusPercentage : 0.04,
+  //   totalCompensation :(1.04 * (employees[0].annualSalary)),
+  //   totalBonus : (0.04 * (employees[0].annualSalary))
+  // }
+
+function newInformation(employee){
+  let Name = employee.name;
+   let bonusPercentage=0;
+   let totalCompensation=0;
+   let totalBonus = 0;
+  
+  if (employees[i].reviewRating <= 2){
+    bonusPercentage=0;
+  }else if(employees[i].reviewRating === 3){
+    bonusPercentage=.04;
+  }else if (employees[i].reviewRating === 4){
+    bonusPercentage=.06;
+  }else if(employees[i].reviewRating === 5){
+    bonusPercentage=.1;
+  }
+
+
+
+
+
+
